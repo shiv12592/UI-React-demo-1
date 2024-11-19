7edd596b98bb410908551dc17f80f844d6d98b28
shiv12592 <shiv.k.jayswal@gmail.com>
2024-11-19 20:31:31 +0530

    attest seperate lables

--- ui-react-demo-1/src/Component/Attestation.jsx
+++ ui-react-demo-1/src/Component/Attestation.jsx
@@ -2,44 +2,75 @@ import React, { Component } from 'react';
 
 class Attestation extends Component {
   state = {
-    conditions: [
-      "I agree to the terms and conditions.",
-      "I confirm that I am above 18 years of age.",
-      "I acknowledge the privacy policy.",
-      "I agree to receive notifications.",
-      "I accept the refund policy.",
-    ],
-    checkboxes: Array(5).fill(false), // Initial state for 5 checkboxes, all unchecked
+    checkboxesForAttest: Array(5).fill(false), // Initial state for 5 checkboxes, all unchecked
   };
 
   handleCheckboxChange = (index) => {
-    const newCheckboxes = [...this.state.checkboxes];
+    const newCheckboxes = [...this.state.checkboxesForAttest];
     newCheckboxes[index] = !newCheckboxes[index]; // Toggle checkbox value
-    this.setState({ checkboxes: newCheckboxes });
+    this.setState({ checkboxesForAttest: newCheckboxes });
   };
 
   areAllChecked = () => {
-    return this.state.checkboxes.every((isChecked) => isChecked);
+    return this.state.checkboxesForAttest.every((isChecked) => isChecked);
   };
 
   render() {
-    const { conditions, checkboxes } = this.state;
+    const { checkboxesForAttest } = this.state;
 
     return (
       <div>
         <h3>Please check all boxes to attest:</h3>
-        {conditions.map((condition, index) => (
-          <div key={index}>
-            <label>
-              <input
-                type="checkbox"
-                checked={checkboxes[index]}
-                onChange={() => this.handleCheckboxChange(index)}
-              />
-              {condition}
-            </label>
-          </div>
-        ))}
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checkboxesForAttest[0]}
+              onChange={() => this.handleCheckboxChange(0)}
+            />
+            I agree to the terms and conditions.
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checkboxesForAttest[1]}
+              onChange={() => this.handleCheckboxChange(1)}
+            />
+            I confirm that I am above 18 years of age.
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checkboxesForAttest[2]}
+              onChange={() => this.handleCheckboxChange(2)}
+            />
+            I acknowledge the privacy policy.
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checkboxesForAttest[3]}
+              onChange={() => this.handleCheckboxChange(3)}
+            />
+            I agree to receive notifications.
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checkboxesForAttest[4]}
+              onChange={() => this.handleCheckboxChange(4)}
+            />
+            I accept the refund policy.
+          </label>
+        </div>
         <button
           onClick={() => alert('Attestation Confirmed!')}
           disabled={!this.areAllChecked()}
